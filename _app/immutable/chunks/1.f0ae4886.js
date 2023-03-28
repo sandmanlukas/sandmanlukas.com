f1e20f1245fd92745b408ae8ee8ed81c0f72bad0
Lukas Sandman <sandmanlukas@gmail.com>
2023-03-28 21:51:25 +0200

    Updates

---
+++ _app/immutable/chunks/1.f0ae4886.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.80a6a946.js";export{t as component};
---
+++ _app/immutable/chunks/4.a7ac9f40.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_page.1806d283.js";import{default as t}from"../entry/whoami-page.svelte.61895b32.js";export{t as component,r as universal};
